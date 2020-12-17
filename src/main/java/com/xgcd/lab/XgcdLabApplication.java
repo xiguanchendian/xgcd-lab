94b2ffc51dd85348311f12e7c4fb35371e9b7356
liangyadong <liangyadong@stargraph.cn>
2020-12-17 15:20:24 +0800

    add file

---
+++ src/main/java/com/xgcd/lab/XgcdLabApplication.java
@@ -0,0 +1,13 @@
+package com.xgcd.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgcdLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XgcdLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgcd/lab/XgcdLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgcd.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XgcdLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
